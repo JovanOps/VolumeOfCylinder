082c5824fc491749142b6c7dc6e52779fea1f7c1
Korisnik <Korisnik@DESKTOP-SGVK0U9>
2025-07-06 01:05:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace VolumeOfCylinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double radius = 10;
+            const double height = 50;
+            const double pi = 3.14;
+
+            double volume = pi * Math.Pow(radius, 2) * height;
+
+            Console.WriteLine("Zapremina valjka sa poluprečnikom {0} i visinom {1} iznosi: {2}", radius, height, volume);
+
+            Console.ReadKey();
+        }
+    }
+}
